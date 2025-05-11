6fd57b8d03bba39846ee2a9663494a1d5b6acff6
SanchitJain28 <mythichuman28@gmail.com>
2025-05-11 11:59:10 +0530

    feat: implement test page with in-app browser detection and redirect popup

---
+++ src/app/(app)/testPage/test.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { RedirectPopup } from "@/app/components/RedirectingPopup";
+import React, { useEffect, useState } from "react";
+
+export default function test() {
+    const [open,setOpen]=useState<boolean>(false)
+  const isInAppBrowser = () => {
+    const ua = navigator.userAgent || navigator.vendor;
+    return /Instagram|FBAN|FBAV/.test(ua);
+  };
+  useEffect(() => {
+    if (isInAppBrowser()) {
+      setOpen(true)
+      return;
+    }
+  }, []);
+
+  return <div>
+    {open && <p className="text-4xl text-black font-bold">You are on Insta</p>}
+    {!open && <p className="text-4xl text-black font-bold">You are on Not on insta broswe</p>}
+    <RedirectPopup ticketUrl="" isOpen={open} onOpenChange={setOpen}/>
+  </div>;
+}
