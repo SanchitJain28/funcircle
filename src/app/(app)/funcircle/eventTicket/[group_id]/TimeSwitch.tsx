cebc7311274fc9b5c88de91d50b70896067f93bd
SanchitJain28 <mythichuman28@gmail.com>
2025-06-22 22:31:38 +0530

    refactor: improve TimeSwitch component layout and readability

--- src/app/(app)/funcircle/eventTicket/[group_id]/TimeSwitch.tsx
+++ src/app/(app)/funcircle/eventTicket/[group_id]/TimeSwitch.tsx
@@ -7,28 +7,29 @@ export default function TimeSwitch({
   onTimeChange,
 }: {
   isMorning: boolean;
-  onTimeChange :(isMorning : boolean) => void
+  onTimeChange: (isMorning: boolean) => void;
 }) {
   return (
     <div>
-      <div className="border-t border-b border-zinc-800/50 mt-6 py-4 px-4">
+      <div className="border-t border-b border-zinc-800/50 mt-6 py-2 px-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
             <Clock className="w-5 h-5 text-purple-400" />
-            <h3 className="text-white font-medium">Time of Day</h3>
+            <h3 className="text-white font-medium">Time of Day : {isMorning ? "Morning" : "Evening"}</h3>
           </div>
           <div className="flex items-center space-x-4">
-            <p className="text-white font-bold">
-              {isMorning ? "Morning" : "Evening"}
-            </p>
-
-            <EventTimeSwitch
-              value={isMorning}
-              defaultStatus={isMorning}
-              onChange={(e) => {
-                onTimeChange(e);
-              }}
-            />
+            <div className="flex flex-col">
+              <p className="text-white text-sm  mb-2">
+                Click to change
+              </p>
+              <EventTimeSwitch
+                value={isMorning}
+                defaultStatus={isMorning}
+                onChange={(e) => {
+                  onTimeChange(e);
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
