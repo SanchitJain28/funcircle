08daea694b9456a4bacda8970131f6796d0fd874
SanchitJain28 <mythichuman28@gmail.com>
2025-07-22 16:59:08 +0530

    feat: enhance sitemap with dynamic venue and group pages
    - Added new API routes to fetch all venues and groups.
    - Updated sitemap generation to dynamically include venue and group pages.
    - This improves SEO by ensuring all venue and group pages are indexed.

---
+++ src/app/api/fetch-all-groups/route.ts
@@ -0,0 +1,45 @@
+import { createClient } from "@/app/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const supabase = await createClient();
+  try {
+    const { data, error } = await supabase.from("groups").select("*");
+    if (error) {
+      return NextResponse.json(
+        {
+          status: false,
+          message: "Database error occured !!",
+          code: "database_error",
+          error,
+        },
+        { status: 401 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        status: true,
+        message: "Success",
+        code: "success",
+        data,
+      },
+      {
+        status: 201,
+        headers: {
+          "Cache-Control": "public, s-maxage=300, stale-while-revalidate=600", // Cache for 5 minutes
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: false,
+        message: "message unexpected error occured",
+        code: "internal_servor_error",
+      },
+      { status: 501 }
+    );
+  }
+}
