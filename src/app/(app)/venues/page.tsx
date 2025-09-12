93b1a4053fcb405782f30e8531561712291e6145
SanchitJain28 <mythichuman28@gmail.com>
2025-09-12 22:10:05 +0530

    changes

--- src/app/(app)/venues/page.tsx
+++ src/app/(app)/venues/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { VenuesClient } from "./VenuesClient";
 
 export default function page() {
   return (
     <div>
-      <VenuesClient />
+      <Suspense fallback={<div>Loading...</div>}>
+        <VenuesClient />
+      </Suspense>
     </div>
   );
 }
